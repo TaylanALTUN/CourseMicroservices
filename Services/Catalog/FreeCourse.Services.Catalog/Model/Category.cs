a2c1df8ac1b9bccdf27fb223339eaed7f0aca7b1
Taylan ALTUN <taylanaltun@gmail.com>
2023-07-24 22:10:11 +0300

    Add project files.

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Model
+{
+    public class Category
+    {
+        // BsonId attribute ile ilgili alanın MongoDb de Id olarak algılanması sağlanır. 
+        // BsonRepresentation attribute ile ilgili alanın tipi belirlenir
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
